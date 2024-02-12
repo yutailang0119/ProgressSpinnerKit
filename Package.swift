59a42ade58bc3742fa63dcae3e79c6844a123f68
yutailang0119 <yutailang0119@gmail.com>
2024-02-12 23:36:16 +0900

    Fix platform to set macOS v10_15

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ProgressSpinnerKit",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         .library(name: "ProgressSpinnerKit", targets: ["ProgressSpinnerKit"]),
     ],
